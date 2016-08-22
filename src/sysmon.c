fc1ba6de6803bccca293fbb0f5bca28d6ec1ba21
emeicoder <zyczar@gmail.com>
2016-08-21 21:14:12 -0700

    add cpu usage

--- src/sysmon.c
+++ src/sysmon.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "sysmon.h"
+
+float calc_cpu_usage()
+{
+	printf("caculating cpu usage\n");
+}
 void main(int argc, char *argv)
 {
 	printf("test %s\n",CPU);
-	printf("test %s\n",MEMORY);
 }
